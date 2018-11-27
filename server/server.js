888489fd0ca5a51cc4b715261df7f0d5993329db
anuragbandhu <anuragbandhu007@gmail.com>
2018-11-27 21:19:37 +0530

    n

--- server/server.js
+++ server/server.js
@@ -57,27 +57,27 @@ app.get('/students/:id', (req, res) => {
 
 });
 
-// app.post('/todos', (req,res) => {
-//     //console.log(req.body);
-//     var todo = new Todo({
-//         text: req.body.text
-//     });
+app.post('/todos', (req,res) => {
+    //console.log(req.body);
+    var todo = new Todo({
+        text: req.body.text
+    });
 
-//     todo.save().then((doc) => {
-//                 //console.log('Saved details', doc);
-//                 res.send(doc);
-//             }, (e) => {
-//                 res.status(400).send(e);
-//             });
-// });
+    todo.save().then((doc) => {
+                //console.log('Saved details', doc);
+                res.send(doc);
+            }, (e) => {
+                res.status(400).send(e);
+            });
+});
 
-// app.get('/todos', (req,res) => {
-//         Todo.find().then((todos) => {
-//             res.send({todos});
-//         }, (e) => {
-//             res.status(400).send(e);
-//         });
-// });
+app.get('/todos', (req,res) => {
+        Todo.find().then((todos) => {
+            res.send({todos});
+        }, (e) => {
+            res.status(400).send(e);
+        });
+});
 
 app.listen(port, () => {
     console.log(`Started up at ${port}`);
